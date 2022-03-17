288d3b71ccd4a061f5c2d6490dde067a38dd273a
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-03-17 17:41:00 +0100

    Add landing page, dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from "react";
 
-import { LogoBlank, LogoTransition } from "@components";
+import { Header, Hero, LogoBlank, LogoTransition } from "@components";
+import { ScaleFade } from '@chakra-ui/react'
 
 const Main: React.FC = () => {
     useEffect(() => { document.body.style.overflow = "hidden"; });
 
     const [done, setDone] = useState(false);
-    setTimeout(() => setDone(true), 10000);
 
     return (
         <React.Fragment>
-            {!done && <LogoBlank fillBall={true} />}
+            {!done && <LogoBlank fillText={true} fillBall={true} setDone={setDone} />}
+            <ScaleFade initialScale={1.0} in={done} >
+                <Header />
+                <Hero />
+            </ScaleFade>
         </React.Fragment>
     );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
+export {Header} from "./headers";
+export {Hero} from "./heros";
 export {LogoTransition} from "./transition";
 export {LogoBlank} from "./transition";
