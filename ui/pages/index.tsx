e8d7d9b905e7626f55b64275e0096280f4ae3b3f
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-03-20 23:42:04 +0100

    Fix api host deduction

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -4,16 +4,20 @@ import { Header, Hero, LogoBlank, LogoTransition } from "@components";
 import { ScaleFade } from '@chakra-ui/react'
 
 const Main: React.FC = () => {
+    const [done, setDone] = useState(false);
+
     let apiHost: string = typeof window !== 'undefined' && window.location.hostname ? window.location.hostname : "";
-    if (process.env.NEXT_PUBLIC_REVIEW_APP) {
+    if (process.env.NEXT_PUBLIC_REVIEW_APP) {    // Heroku Review Apps
         apiHost = apiHost.replace("booking-ui", "booking-api");
-    } else if (typeof window !== 'undefined' && window.location.hostname === 'localhost') {
+    } else if (typeof window !== 'undefined' && window.location.hostname === 'localhost') { // Local integration testing
         apiHost = "localhost:5000";
     } else {
-        apiHost = "api." + apiHost;
+        if (process.env.NEXT_PUBLIC_API_HOST) {
+            apiHost = process.env.NEXT_PUBLIC_API_HOST;
+        } else {
+            return <p>An error occurred reading API HOST domain from environment</p>
+        }
     }
-
-    const [done, setDone] = useState(false);
 
     if (!done) {
         return <LogoBlank fillText={true} fillBall={true} setDone={setDone} />;
