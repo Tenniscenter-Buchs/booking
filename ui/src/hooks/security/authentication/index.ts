416ab5b8303f5a72f61c2cb5920e27831b73a6d2
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-03-30 02:08:46 +0200

    Fix static site generation

--- ui/src/hooks/security/authentication/index.ts
+++ ui/src/hooks/security/authentication/index.ts
@@ -3,6 +3,7 @@ import Session from 'supertokens-auth-react/recipe/session';
 
 export const useLoginStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false);
+    if (typeof window === 'undefined') return;
     const waitForSession = async () => {
         setLoggedIn(await Session.doesSessionExist());
     }
