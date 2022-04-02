e5271cf1a40e40cb373f57dde9cd3b8d5b0639bc
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-04-02 22:31:48 +0200

    Use next router

--- ui/src/components/headers/profile.tsx
+++ ui/src/components/headers/profile.tsx
@@ -1,11 +1,12 @@
 import { IconButton } from "@chakra-ui/react";
+import Router from "next/router";
 import { FaUser } from "react-icons/fa";
 import { signOut } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
 
 export function ProfileMenu() {
     async function onLogout() {
         await signOut();
-        window.location.href = "/";
+        Router.push("/");
     }
     const LogoutButton = (
         <IconButton
