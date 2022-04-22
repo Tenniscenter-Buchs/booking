cd33caeae090db77072d5ed43df347339e5413c4
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-04-22 22:42:23 +0200

    Add audit_log table

---
+++ api/src/entity/Audit.ts
@@ -0,0 +1,43 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, Timestamp } from 'typeorm';
+import { User } from './User';
+
+export enum AuditEntryEvent {
+    USER_SIGNUP = 'user_signup',
+        USER_INCONSISTENT_SIGNUP = 'user_inconsistent_signup'
+}
+
+export enum AuditEntryLevel {
+    VERBOSE = 'verbose',
+        DEBUG = 'debug',
+        INFO = 'info',
+        WARNING = 'warning',
+        ERROR = 'error',
+        FATAL = 'fatal'
+}
+
+@Entity('audit_log')
+export class AuditEntry {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => User)
+    user: User;
+
+    @Column({
+        type: 'enum',
+        enum: AuditEntryEvent,
+    })
+    type: AuditEntryEvent;
+
+    @Column({
+        type: 'enum',
+        enum: AuditEntryLevel,
+    })
+    level: AuditEntryLevel;
+
+    @Column()
+    detail: string;
+
+    @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+    time: Timestamp;
+}
